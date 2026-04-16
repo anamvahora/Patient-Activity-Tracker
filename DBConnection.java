633877d0d4bb54c2e09a11314091072c033a4911
Asalgado17 <114386063+Asalgado17@users.noreply.github.com>
2026-04-16 20:08:50 +0000

    Removed Student 1 dbutils class combined content into DBCOnnection class changed forms medicalhistory and patient demographics to reflect that class change.

--- DBConnection.java
+++ DBConnection.java
@@ -1,16 +1,21 @@
-package DBUtils;
+package its340_andreas_anamv;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConnection {
+    
+    // lets student 2 keep calling DBConnection.getConnection() without changes.
     public static Connection getConnection() {
+        // Just calls the second method below with the default credentials
+        return getConnection("root", "Password"); 
+    }
+
+    // This lets student 1 pass in "root" and "" (empty) or any other credentials
+    public static Connection getConnection(String username, String password) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            return DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/PatientDB",
-                "root",
-                "Password"
-            );
+            String url = "jdbc:mysql://localhost:3306/medical";
+            return DriverManager.getConnection(url, username, password);
         } catch (Exception e) {
             e.printStackTrace();
             return null;
